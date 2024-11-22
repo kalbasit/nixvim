f17dcbca3e6b8164d75e651e22ba22499a8aef95
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-22 00:31:18 -0800

    config/keymaps: update NeoTree to work better with Colemak

--- config/keymaps/colemak.nix
+++ config/keymaps/colemak.nix
@@ -1,3 +1,41 @@
 {
   plugins.vim-colemak.enable = true;
+
+  plugins.neo-tree = {
+    # Disable the default keymappings
+    useDefaultMappings = false;
+
+    window.mappings = {
+      "<CR>" = "open";
+      "<esc>" = "revert_preview";
+      P = {
+        command = "toggle_preview";
+        config = {
+          use_float = true;
+        };
+      };
+      l = "focus_preview";
+      s = "open_split";
+      S = "open_vsplit";
+      t = "open_tabnew";
+      R = "refresh";
+      a = {
+        command = "add";
+        # some commands may take optional config options, see `:h neo-tree-mappings` for details
+        config = {
+          show_path = "none"; # "none", "relative", "absolute"
+        };
+      };
+      A = "add_directory"; # also accepts the config.show_path and config.insert_as options.
+      d = "delete";
+      r = "rename";
+      c = "copy_to_clipboard";
+      x = "cut_to_clipboard";
+      v = "paste_from_clipboard";
+      q = "close_window";
+      "?" = "show_help";
+      "<" = "prev_source";
+      ">" = "next_source";
+    };
+  };
 }
