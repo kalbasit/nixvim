e1871916753c89de6b380334b9bf4e732bebfc1b
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-21 20:36:43 -0800

    config/plugins/cmp: adjust mapping for colemak

--- config/plugins/cmp/cmp.nix
+++ config/plugins/cmp/cmp.nix
@@ -56,9 +56,9 @@
 
         mapping = {
           "<C-Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<C-j>" = "cmp.mapping.select_next_item()";
-          "<C-k>" = "cmp.mapping.select_prev_item()";
-          "<C-e>" = "cmp.mapping.abort()";
+          "<C-e>" = "cmp.mapping.select_next_item()";
+          "<C-i>" = "cmp.mapping.select_prev_item()";
+          "<C-c>" = "cmp.mapping.abort()";
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
@@ -111,7 +111,7 @@
           Event = " ",
           Operator = " ",
           TypeParameter = " ",
-        } 
+        }
 
          local cmp = require'cmp'
 
