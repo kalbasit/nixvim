cee38f50b5a677411f164e2844bde66a0b7c7e9f
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-21 23:18:04 -0800

    flake: Update the description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "deCort.tech NeoVim configuration";
+  description = "Wael Nasreddine (kalbasit) NeoVim configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
