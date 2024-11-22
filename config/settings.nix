d12704d3740efe5b4911c3683cae41ed1b12027c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-21 19:25:38 -0800

    config/settings: remember things like marks and registers between sessions

--- config/settings.nix
+++ config/settings.nix
@@ -72,6 +72,15 @@
       # Remember undo chains between sessions
       undofile = true;
 
+      # Remember things between sessions
+      #
+      # '20  - remember marks for 20 previous files
+      # <50 - save 50 lines for each register
+      # :20  - remember 20 items in command-line history
+      # %    - remember the buffer list (if vim started without a file arg)
+      # n    - set name of viminfo file
+      shada = "'20,<50,:20,%,n~/.config/nvim/_nviminfo";
+
       # Don't show mode in the command line
       showmode = false;
 
