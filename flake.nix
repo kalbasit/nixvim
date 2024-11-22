20796603fff988f92b3c113820b1f9d3d34875b5
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-21 23:22:08 -0800

    flake: all inputs should follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,12 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
+
     nixvim = {
+      inputs.nixpkgs.follows = "nixpkgs";
       # url = "github:nix-community/nixvim";
       url = "github:kalbasit/nix-community_nixvim/colemak";
     };
+
     pre-commit-hooks = {
+      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:cachix/pre-commit-hooks.nix";
     };
   };
