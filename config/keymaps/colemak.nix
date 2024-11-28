ae185c37deb5d10ee708cbfb333d66955987718e
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-11-27 20:01:43 -0800

    config/keymaps: Disable indentscope mapping ii
    The mapping ii happens to be my kk mapping so I hit it when I try to go
    up. Disable it so it does not get in the way.

--- config/keymaps/colemak.nix
+++ config/keymaps/colemak.nix
@@ -1,41 +1,53 @@
 {
-  plugins.vim-colemak.enable = true;
+  plugins = {
+    vim-colemak.enable = true;
 
-  plugins.neo-tree = {
-    # Disable the default keymappings
-    useDefaultMappings = false;
-
-    window.mappings = {
-      "<CR>" = "open";
-      "<esc>" = "revert_preview";
-      P = {
-        command = "toggle_preview";
-        config = {
-          use_float = true;
+    mini = {
+      modules = {
+        indentscope = {
+          mappings = {
+            object_scope = "";
+          };
         };
       };
-      l = "focus_preview";
-      s = "open_split";
-      S = "open_vsplit";
-      t = "open_tabnew";
-      R = "refresh";
-      a = {
-        command = "add";
-        # some commands may take optional config options, see `:h neo-tree-mappings` for details
-        config = {
-          show_path = "none"; # "none", "relative", "absolute"
+    };
+
+    neo-tree = {
+      # Disable the default keymappings
+      useDefaultMappings = false;
+
+      window.mappings = {
+        "<CR>" = "open";
+        "<esc>" = "revert_preview";
+        P = {
+          command = "toggle_preview";
+          config = {
+            use_float = true;
+          };
         };
+        l = "focus_preview";
+        s = "open_split";
+        S = "open_vsplit";
+        t = "open_tabnew";
+        R = "refresh";
+        a = {
+          command = "add";
+          # some commands may take optional config options, see `:h neo-tree-mappings` for details
+          config = {
+            show_path = "none"; # "none", "relative", "absolute"
+          };
+        };
+        A = "add_directory"; # also accepts the config.show_path and config.insert_as options.
+        d = "delete";
+        r = "rename";
+        c = "copy_to_clipboard";
+        x = "cut_to_clipboard";
+        v = "paste_from_clipboard";
+        q = "close_window";
+        "?" = "show_help";
+        "<" = "prev_source";
+        ">" = "next_source";
       };
-      A = "add_directory"; # also accepts the config.show_path and config.insert_as options.
-      d = "delete";
-      r = "rename";
-      c = "copy_to_clipboard";
-      x = "cut_to_clipboard";
-      v = "paste_from_clipboard";
-      q = "close_window";
-      "?" = "show_help";
-      "<" = "prev_source";
-      ">" = "next_source";
     };
   };
 }
