9edfb03e180b0709d15ab416abea7c7e85aaaec3
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-02-23 14:18:34 -0800

    config/plugins/ui/startup: Make logo configurable

--- config/plugins/ui/startup.nix
+++ config/plugins/ui/startup.nix
@@ -1,84 +1,96 @@
+{ config, lib, ... }:
+
 {
-  plugins.startup = {
-    enable = true;
-
-    colors = {
-      background = "#ffffff";
-      foldedSection = "#ffffff";
+  options = {
+    neoVimStartupLogo = lib.mkOption {
+      type = lib.types.str;
+      # Use https://fsymbols.com/generators/carty/ to generate this art
+      default = ''
+        ████████╗██████╗░██╗██████╗░██╗███╗░░██╗░░░████████╗███████╗░█████╗░██╗░░██╗
+        ╚══██╔══╝██╔══██╗██║██╔══██╗██║████╗░██║░░░╚══██╔══╝██╔════╝██╔══██╗██║░░██║
+        ░░░██║░░░██████╔╝██║██████╔╝██║██╔██╗██║░░░░░░██║░░░█████╗░░██║░░╚═╝███████║
+        ░░░██║░░░██╔══██╗██║██╔═══╝░██║██║╚████║░░░░░░██║░░░██╔══╝░░██║░░██╗██╔══██║
+        ░░░██║░░░██║░░██║██║██║░░░░░██║██║░╚███║██╗░░░██║░░░███████╗╚█████╔╝██║░░██║
+        ░░░╚═╝░░░╚═╝░░╚═╝╚═╝╚═╝░░░░░╚═╝╚═╝░░╚══╝╚═╝░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝
+      '';
+      description = "Logo";
     };
+  };
 
-    sections = {
-      header = {
-        type = "text";
-        oldfilesDirectory = false;
-        align = "center";
-        foldSection = false;
-        title = "Header";
-        margin = 5;
-        content = [
-          # Use https://fsymbols.com/generators/carty/ to generate this art
-          "████████╗██████╗░██╗██████╗░██╗███╗░░██╗░░░████████╗███████╗░█████╗░██╗░░██╗"
-          "╚══██╔══╝██╔══██╗██║██╔══██╗██║████╗░██║░░░╚══██╔══╝██╔════╝██╔══██╗██║░░██║"
-          "░░░██║░░░██████╔╝██║██████╔╝██║██╔██╗██║░░░░░░██║░░░█████╗░░██║░░╚═╝███████║"
-          "░░░██║░░░██╔══██╗██║██╔═══╝░██║██║╚████║░░░░░░██║░░░██╔══╝░░██║░░██╗██╔══██║"
-          "░░░██║░░░██║░░██║██║██║░░░░░██║██║░╚███║██╗░░░██║░░░███████╗╚█████╔╝██║░░██║"
-          "░░░╚═╝░░░╚═╝░░╚═╝╚═╝╚═╝░░░░░╚═╝╚═╝░░╚══╝╚═╝░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝"
-        ];
-        highlight = "Statement";
-        defaultColor = "";
-        oldfilesAmount = 0;
+  config = {
+    plugins.startup = {
+      enable = true;
+
+      colors = {
+        background = "#ffffff";
+        foldedSection = "#ffffff";
       };
 
-      body = {
-        type = "mapping";
-        oldfilesDirectory = false;
-        align = "center";
-        foldSection = false;
-        title = "Menu";
-        margin = 5;
-        content = [
-          [
-            " Find File"
-            "Telescope find_files"
-            "<leader>ff"
-          ]
-          [
-            "󰍉 Find Word"
-            "Telescope live_grep"
-            "<leader>fr"
-          ]
-          [
-            " Recent Files"
-            "Telescope oldfiles"
-            "<leader>fr"
-          ]
-          [
-            " File Browser"
-            "Telescope file_browser"
-            "<leader>fe"
-          ]
-          [
-            " Copilot Chat"
-            "CopilotChat"
-            "<leader>ct"
-          ]
-        ];
-        highlight = "string";
-        defaultColor = "";
-        oldfilesAmount = 0;
-      };
-    };
+      sections = {
+        header = {
+          type = "text";
+          oldfilesDirectory = false;
+          align = "center";
+          foldSection = false;
+          title = "Header";
+          margin = 5;
+          content = lib.strings.splitString "\n" config.neoVimStartupLogo;
+          highlight = "Statement";
+          defaultColor = "";
+          oldfilesAmount = 0;
+        };
 
-    options = {
-      paddings = [
-        1
-        3
+        body = {
+          type = "mapping";
+          oldfilesDirectory = false;
+          align = "center";
+          foldSection = false;
+          title = "Menu";
+          margin = 5;
+          content = [
+            [
+              " Find File"
+              "Telescope find_files"
+              "<leader>ff"
+            ]
+            [
+              "󰍉 Find Word"
+              "Telescope live_grep"
+              "<leader>fr"
+            ]
+            [
+              " Recent Files"
+              "Telescope oldfiles"
+              "<leader>fr"
+            ]
+            [
+              " File Browser"
+              "Telescope file_browser"
+              "<leader>fe"
+            ]
+            [
+              " Copilot Chat"
+              "CopilotChat"
+              "<leader>ct"
+            ]
+          ];
+          highlight = "string";
+          defaultColor = "";
+          oldfilesAmount = 0;
+        };
+      };
+
+      options = {
+        paddings = [
+          1
+          3
+        ];
+      };
+
+      parts = [
+        "header"
+        "body"
       ];
     };
-
-    parts = [
-      "header"
-      "body"
-    ];
   };
 }
